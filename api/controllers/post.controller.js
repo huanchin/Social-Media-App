61436f1529588ace089b39aea1bab5782413afa0
huan-chin chen <cherry032826@gmail.com>
2024-04-23 14:58:32 +0800

    posts order by create date

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -4,8 +4,8 @@ export const getPosts = async (req, res, next) => {
   try {
     const userId = req.session.userId;
     const query =
-      "SELECT p.*, u.id AS userId, username, profile FROM posts AS p INNER JOIN users AS u ON (u.id = p.user_id) INNER JOIN relationships AS r ON (p.user_id = r.followed_user_id AND r.follower_user_id = ?)";
-    const [posts] = await pool.query(query, [userId]);
+      "SELECT p.*, u.id AS userId, username, profile FROM posts AS p INNER JOIN users AS u ON (u.id = p.user_id) LEFT JOIN relationships AS r ON p.user_id = r.followed_user_id WHERE r.follower_user_id = ? OR p.user_id = ? ORDER BY p.createdAt DESC";
+    const [posts] = await pool.query(query, [userId, userId]);
     // console.log(posts);
     res.status(200).json(posts);
   } catch (err) {
