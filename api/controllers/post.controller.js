112744726d3f7ca5ab639c2187445b61d02722f0
huan-chin chen <cherry032826@gmail.com>
2024-04-23 15:21:55 +0800

    add create post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,4 +1,5 @@
 import pool from "../database.js";
+import moment from "moment";
 
 export const getPosts = async (req, res, next) => {
   try {
@@ -8,6 +9,25 @@ export const getPosts = async (req, res, next) => {
     const [posts] = await pool.query(query, [userId, userId]);
     // console.log(posts);
     res.status(200).json(posts);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const createPost = async (req, res, next) => {
+  const { desc, img } = req.body;
+  try {
+    const userId = req.session.userId;
+    const query =
+      "INSERT INTO `social`.`posts` ( `user_id`, `desc`, `img`, `createdAt`) VALUES ( ?, ?, ?, ?);";
+    const newPost = [
+      userId,
+      desc,
+      img,
+      moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+    ];
+    await pool.query(query, newPost);
+    res.status(201).json("post has been created");
   } catch (err) {
     next(err);
   }
--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getPosts } from "../controllers/post.controller.js";
+import { getPosts, createPost } from "../controllers/post.controller.js";
 import verifySession from "../utils/verifySession.js";
 
 const router = express.Router();
 
 router.get("/", verifySession, getPosts);
+router.post("/create", verifySession, createPost);
 
 export default router;
